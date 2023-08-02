c15622cadbf124c01666948e09a7b5732d78edf6
Nikhil <98229390+nikhilgurung@users.noreply.github.com>
2023-08-02 18:35:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import random
+
+def get_user_choice():
+    while True:
+        user_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+        if user_choice in ["rock", "paper", "scissors"]:
+            return user_choice
+        else:
+            print("Invalid choice. Please enter 'rock', 'paper', or 'scissors'.")
+
+def get_computer_choice():
+    choices = ["rock", "paper", "scissors"]
+    return random.choice(choices)
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif user_choice == "rock":
+        return "You win!" if computer_choice == "scissors" else "Computer wins!"
+    elif user_choice == "paper":
+        return "You win!" if computer_choice == "rock" else "Computer wins!"
+    else:  # user_choice == "scissors"
+        return "You win!" if computer_choice == "paper" else "Computer wins!"
+
+def main():
+    print("Welcome to Rock-Paper-Scissors!")
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        print(f"\nYou chose: {user_choice}")
+        print(f"Computer chose: {computer_choice}")
+        print(determine_winner(user_choice, computer_choice))
+
+        play_again = input("\nDo you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            break
+
+    print("Thanks for playing!")
+
+if __name__ == "__main__":
+    main()
